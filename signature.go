525cb09240349fd6ea6e660a070e1a7864f9b661
billy <me@ybilly.com>
2022-03-30 18:04:01 +0800

    验证签名

---
+++ signature.go
@@ -0,0 +1,19 @@
+package weixinapi
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// 验证签名是否合法
+func ValidateSignature(tok, timestamp, nonce, signature string) bool {
+	strs := []string{tok, timestamp, nonce}
+	sort.Strings(strs)
+
+	tmpStr := strings.Join(strs, "")
+	actual := fmt.Sprintf("%x", sha1.Sum([]byte(tmpStr)))
+
+	return actual == signature
+}
